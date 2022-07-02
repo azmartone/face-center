e6a57d66c3dcfbe02ab08f0b5e7469f9dc404dc7
azmartone <dannyduong888@gmail.com>
2022-07-02 15:53:23 -0700

    feat(detection): add face detection demo app

--- src/App.js
+++ src/App.js
@@ -1,41 +1,63 @@
-import React, {useRef}  from 'react';
-import './App.css';
-import styled from 'styled-components';
-import {useCallbackRef} from "use-callback-ref";
-import 'face-api.js'
+import React, { useEffect, useRef }  from 'react'
+import './App.css'
+import styled from 'styled-components'
+import * as faceapi from 'face-api.js'
+// import { Input } from '@chakra-ui/react'
 
 const MainCanvas = styled.canvas`
     border: 1px solid red;
 `
+const MODEL_URL = '/models'
 
 function App() {
+    const inputRef = useRef()
+    const imageRef = useRef()
+    const canvasRef = useRef()
 
-Promise.all([
-    faceapi.nets.faceRecognitionNet.loadFromUri('images/face/'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('images/face/'),
-    faceapi.nets.ssdMobilenetv1.loadFromUri('images/face/')
-]).then(start)
-
-    function start(){
-    console.log('loadedd')
+    const start = ()=>{
+        console.log('loadedd')
     }
 
-    // const loadImage = ()=> {
-    //     var imageObj1 = new Image();
-    //     imageObj1.src = 'images/face/MVIMG_20190918_084958.jpg'
-    //     imageObj1.onload = handleLoaded
-    // }
+    useEffect(()=>{
+        Promise.all([
+            faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+            faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+            faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_URL),
+        ]).then(start)
+    }, [])
 
-    // const canvasEl = useCallbackRef(null, loadImage);
+    const handleChange = async ()=>{
+        console.log('handleChange')
+        const image = await faceapi.bufferToImage(inputRef.current.files[0])
+        imageRef.current.src = image.src
+
+        const apiCanvas = faceapi.createCanvasFromMedia(image)
+
+        const displaySize = { width: image.width, height: image.height }
+        faceapi.matchDimensions(apiCanvas, displaySize)
+
+        const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
+
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+
+        resizedDetections.forEach( detection => {
+            const box = detection.detection.box
+            const drawBox = new faceapi.draw.DrawBox(box, { label : 'Face' })
+            drawBox.draw(apiCanvas)
+        })
+
+        canvasRef.current.width = apiCanvas.width
+        canvasRef.current.height = apiCanvas.height
+        const destCtx = canvasRef.current.getContext('2d')
+        destCtx.drawImage(apiCanvas, 0, 0)
+    }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="images/face/20200103_221942.jpg" className="App-logo" alt="logo" />
-        <MainCanvas id="mainCanvas" width="500" height="500"     />
-      </header>
+        <input ref={inputRef} type="file" id="ImageUpload" onChange={handleChange}/>
+        <img ref={imageRef}/><canvas ref={canvasRef}></canvas>
     </div>
-  );
+  )
 }
 
 export default App;
